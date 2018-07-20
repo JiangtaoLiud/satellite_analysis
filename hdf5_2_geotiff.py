fc2896045b9c5eb792ee421a39b784f215aea789
Zepy1 <m.pezij@utwente.nl>
2018-07-20 15:24:36 +0200

    cleaned file

--- hdf5_2_geotiff.py
+++ hdf5_2_geotiff.py
@@ -14,13 +14,9 @@ gdal.AllRegister()
 ###############################################################################
 # read data
 ###############################################################################
-#dir_name = r"f:\data\smap\download\SMAP_Twente\\hdf5\\"
-#fNames = glob(dir_name+"*.h5")
-
-#fNames = [r'SMAP_L3_SM_P_20180703_R16010_001.h5']
-#fNames = [r'SMAP_L3_SM_P_E_20180703_R16010_001.h5']
 
 fNames = glob('*.h5')
+
 for i in fNames:
     with h5py.File(i, mode="r") as f:
         name = '/Soil_Moisture_Retrieval_Data_AM/soil_moisture'
@@ -43,21 +39,13 @@ for i in fNames:
         latitude_masked = np.ma.masked_where(latitude==-9999, latitude)
         longitude_masked = np.ma.masked_where(longitude==-9999, longitude)
 
-#%%
         # reproject coordinates
-
-
         inProj = Proj(init='epsg:4326')
-#        outProj = Proj(init='epgs:6933')
         outProj = Proj('+proj=cea +lon_0=0 +lat_ts=30 +x_0=0 +y_0=0 +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs')
-        
-        
-        
+
         longitude,latitude = transform(inProj, outProj, longitude_masked, latitude_masked)
         
-        
-        
-        # setup geotransform
+        # set up geotransform
         num_cols = data.shape[1]
         num_rows = data.shape[0]        
         
@@ -71,16 +59,11 @@ for i in fNames:
         
         geotransform = (xmin, xres, 0, ymax, 0, -yres)
         
-        #        # create geotiff
+        # create geotiff
         directory, filename = os.path.split(i)
         filename_ext = os.path.splitext(filename)[0]
         driver = gdal.GetDriverByName("Gtiff")
-        
-#        src_ds = gdal.Open("SMAP_L3_SM_P_E_20150401_R14010_001.tif")
-        
-#        raster = driver.CreateCopy(r"geotiffs\\"+filename_ext+".tif", src_ds, strict=0)
-#                src_ds = None
-
+       
         raster = driver.Create(os.path.splitext(i)[0] + ".tif",
                                int(num_cols), int(num_rows),
                                1, gdal.GDT_Float32)
@@ -88,12 +71,8 @@ for i in fNames:
         # set geotransform and projection
         raster.SetGeoTransform(geotransform)
         srs = osr.SpatialReference()
-#        srs.ImportFromEPSG(4326)
         wkt_text = r'PROJCS["unnamed",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9108"]],AUTHORITY["EPSG","4326"]],PROJECTION["Cylindrical_Equal_Area"],PARAMETER["standard_parallel_1",30],PARAMETER["central_meridian",0],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["Meter",1],AUTHORITY["epsg","6933"]]'
-        
         srs.ImportFromWkt(wkt_text)
-        
-         
         raster.SetProjection(srs.ExportToWkt())
         
         # write data array to raster
